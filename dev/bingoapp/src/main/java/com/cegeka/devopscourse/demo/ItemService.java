186d92473f3214c80e3fdfe2f08bca0d802a5d1b
Frank Kunnen <frank_kunnen@hotmail.com>
2017-11-28 14:42:27 +0100

    jenkinsfile trigger test

--- dev/bingoapp/src/main/java/com/cegeka/devopscourse/demo/ItemService.java
+++ dev/bingoapp/src/main/java/com/cegeka/devopscourse/demo/ItemService.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class ItemService {
 
     @Autowired
-    private ItemRepository itemRepository;
+    private ItemRepository  itemRepository;
 
     public void saveItem(Item item) {
         itemRepository.save(item);
