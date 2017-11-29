2521d1538e6fee05902840aac1f022563a4a85e4
jelledrm <jelle2510@gmail.com>
2017-11-29 12:22:32 +0100

    sleep

--- dev/smoketest/src/smoketest/java/BingoSmokeTest.java
+++ dev/smoketest/src/smoketest/java/BingoSmokeTest.java
@@ -4,9 +4,9 @@ import org.junit.Test;
 public class BingoSmokeTest {
 
     @Test
-    public void givenAStartedApplication_whenAskingForTheStatusPage_thenReturnHttp200(){
+    public void givenAStartedApplication_whenAskingForTheStatusPage_thenReturnHttp200() throws InterruptedException {
+        Thread.sleep(30000);
         RestAssured.when().get("http://localhost:8888/bingo/health").then()
                 .statusCode(200);
     }
-
 }
